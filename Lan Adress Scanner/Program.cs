9c0d176bf8c1a52194f9483e7002504e5bbb45b1
Ali Kanıberk <thtramboreis1@gmail.com>
2021-08-16 19:26:57 +0300

    Proje dosyası ekle.

---
+++ Lan Adress Scanner/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lan_Adress_Scanner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Title = "Lan Adres Ve İndex Tarayıcı";
+            List<string> aktifler = new List<string>();
+            for (; ; )
+            {
+                aktifler.Clear();
+                Console.Clear();
+                Stopwatch stop = new Stopwatch();
+                stop.Start();
+                Console.WriteLine("Website : kodzamani.weebly.com");
+                Console.WriteLine("Instagram : @kodzamani.tk");
+                Console.Write("Alt internet adresi ( Varsayılan 192.168.1 ) :");
+                string altadres = Console.ReadLine();
+                if (altadres == "")
+                    altadres = "192.168.1";
+                Console.WriteLine("--------------------İp Adresleri--------------------");
+                Parallel.For(2, 255, i =>
+                  {
+                      try
+                      {
+                          string ip = $"{altadres}.{i}";
+                          Ping ping = new Ping();
+                          PingReply reply = ping.Send(ip, 100);
+                          if (reply.Status == IPStatus.Success)
+                          {
+                              Console.WriteLine(ip + " > Bağlantı Var");
+                              aktifler.Add(ip);
+                          }
+                          else
+                              Console.WriteLine(ip + " > Bağlantı Yok");
+                      }
+                      catch
+                      {
+                          Console.WriteLine($"{altadres}.{i}" + " > Bağlantı Hatası");
+                      }
+                  });
+                Console.WriteLine("--------------------İndexler--------------------");
+                if (aktifler.Count == 0)
+                    Console.WriteLine("Hiçbir açık bağlantı bulunamadı.");
+                Parallel.ForEach(aktifler, aktif =>
+                 {
+                     try
+                     {
+                         HttpWebRequest request = (HttpWebRequest)WebRequest.Create("http://" + aktif);
+                         HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+                             Console.WriteLine(aktif + " > İndex Var");
+                     }
+                     catch { Console.WriteLine(aktif + " > İndex Yok"); }
+                 });
+                stop.Stop();
+                Console.WriteLine("--------------------Sonuç--------------------");
+                Console.WriteLine("Geçen süre saniye : " + stop.Elapsed.TotalSeconds);
+                Console.WriteLine("Tüm işlemler bitirildi. tekrar taramak için bir tuşa basın.");
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ Lan Adress Scanner/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Bir bütünleştirilmiş koda ilişkin Genel Bilgiler aşağıdaki öznitelikler kümesiyle
+// denetlenir. Bütünleştirilmiş kod ile ilişkili bilgileri değiştirmek için
+// bu öznitelik değerlerini değiştirin.
+[assembly: AssemblyTitle("Lan Adress Scanner")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Lan Adress Scanner")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible özniteliğinin false olarak ayarlanması bu bütünleştirilmiş koddaki türleri
+// COM bileşenleri için görünmez yapar. Bu bütünleştirilmiş koddaki bir türe
+// erişmeniz gerekirse ComVisible özniteliğini o türde true olarak ayarlayın.
+[assembly: ComVisible(false)]
+
+// Bu proje COM'un kullanımına sunulursa, aşağıdaki GUID tür kitaplığının kimliği içindir
+[assembly: Guid("0f8c93cf-1fb6-4d31-81a6-9198343e4101")]
+
+// Bir derlemenin sürüm bilgileri aşağıdaki dört değerden oluşur:
+//
+//      Ana Sürüm
+//      İkincil Sürüm 
+//      Yapı Numarası
+//      Düzeltme
+//
+// Tüm değerleri belirtebilir veya varsayılan Derleme ve Düzeltme Numaralarını kullanmak için
+// aşağıda gösterildiği gibi '*' kullanabilirsiniz:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
